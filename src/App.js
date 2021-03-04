ab344735d47809886717bcb684ce64fc58177f07
Sajal Goyal <35042565+goel5@users.noreply.github.com>
2021-03-04 18:50:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ import "./App.css";
 function App() {
   const [value, setValue] = useState("")
   const [results, setResults]=useState([])
-
-  const fetchImages = ()=>{
-    fetch(`https://api.unsplash.com/search/photos?client_id=3IvfH2FWSe4PZws1QOxsLtBRpdAlVeGWvuvH3CF2sHc&query=${value}&orientation=landscape&per_page=16`)
+  const [pageNo, setPageNo]=useState(1);
+  const fetchImages = (flag)=>{
+    if(flag){
+      fetch(`https://api.unsplash.com/search/photos?client_id=3IvfH2FWSe4PZws1QOxsLtBRpdAlVeGWvuvH3CF2sHc&query=${value}&orientation=landscape&per_page=16&page=1`)
     .then(res=>res.json())
     .then(data => {
       setResults(data.results)
-    })
+      setPageNo(2)
+    });
+    }
+    else{
+      fetch(`https://api.unsplash.com/search/photos?client_id=3IvfH2FWSe4PZws1QOxsLtBRpdAlVeGWvuvH3CF2sHc&query=${value}&orientation=landscape&per_page=16&page=${pageNo}`)
+    .then(res=>res.json())
+    .then(data => {
+      let tempData=results
+      // console.log(tempData.length)
+      tempData=tempData.concat(data.results)
+      // console.log(tempData.length)
+      setResults(tempData)
+      setPageNo(pageNo+1)
+    });
+    } 
+    
   }
     return (
       <div className="App">
         <div className="mydiv">
           <input type="text" placeholder="Search for photos" value={value} onChange={(e)=>setValue(e.target.value)}/>
-          <button className="search" onClick={()=>fetchImages()}>Search</button>
+          <button className="search" onClick={()=>fetchImages(true)}>Search</button>
         </div>
         <div className="gallery">
           {
@@ -26,7 +42,7 @@ function App() {
             })
           }
         </div>
-        <button id="load">Load more</button>
+        <button id="load" onClick={()=>fetchImages(false)}>Load more</button>
       </div>
     );
 }
